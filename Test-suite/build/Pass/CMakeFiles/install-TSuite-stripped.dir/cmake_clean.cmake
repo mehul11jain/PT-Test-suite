7df6f223dd9b1b702317d4dd8e8f83f529159679
Mehul Jain <mehul11jain@outlook.com>
2021-03-28 00:47:30 +0530

    gitignore added

--- Test-suite/build/Pass/CMakeFiles/install-TSuite-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-TSuite-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-TSuite-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
